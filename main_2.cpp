79287448ebef76a0ea33b1854e0e0629bb2f6756
NotSaqlain <iamnotsaqlain@gmail.com>
2024-03-31 20:54:25 +0200

    update

--- main_2.cpp
+++ main_2.cpp
@@ -7,14 +7,12 @@ using namespace std;
 
 const int N = 10;
 
-void stampa(int array[]) {
-    for (int i = 0; i < N; i++) {
-        cout << array[i] << endl;
-    }
-}
+void stampa(int array[]);
+int massimo(int array[], int max, int index);
 
 int main() {
     int array[N];
+    int max = array[0];
     
     for (int i = 0; i < N; i++) {
         array[i] = rand() % 100;
@@ -22,5 +20,22 @@ int main() {
     
     stampa(array);
 
+    cout << "Numero massimo: " << massimo(array, max, 0) << endl;
+
     return 0;
+}
+
+void stampa(int array[]) {
+    for (int i = 0; i < N; i++) {
+        cout << array[i] << endl;
+    }
+}
+
+int massimo(int array[], int max, int index) {
+    if (index >= N) return max;
+
+    if (max < array[index]) max = array[index];
+
+    massimo(array, max, index+1);
+
 }
