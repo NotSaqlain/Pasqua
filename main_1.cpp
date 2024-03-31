d1d4c9cd5c5178ac4e2b5e1e091d07cff085722b
NotSaqlain <iamnotsaqlain@gmail.com>
2024-03-31 19:10:04 +0200

    update

--- main_1.cpp
+++ main_1.cpp
@@ -6,32 +6,33 @@
 
 using namespace std;
 
-// int numOccorenza(string frase, char c);
+int numOccorenza(string frase, char c);
+
 
 int main() {
-  string s;
-  char c;
-  
-  cout << "Scrivi la frse: ";
-  cin >> s;
+    string s;
+    char c;
 
-  int len = s.size();
-  cout << len;
+    cout << "Scrivi la frase: ";
+    cin >> s;
 
-  cout << "Scrivi un carattere: ";
-  cin >> c;
+    cout << "Scrivi un carattere: ";
+    cin >> c;
 
+    cout << c << " e presente " << numOccorenza(s, c) << "volte nella frase!" << endl;
 
-
-
-
-  return 0;
+    return 0;
 }
 
-/*
+
 int numOccorenza(string frase, char c) {
-  if (frase[0] != '0') {
-    return ;
-  }
+    int start = 0;
+    int end = frase.size();
+
+    if (start == end) {
+        return 0;
+    } else if (frase[start] == c) {
+        frase.erase(0, 1);
+        return numOccorenza(frase, c);
+    }
 }
-*/
