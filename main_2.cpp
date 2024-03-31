d4ff333aad8589f6dd27018c6a55ae4cfd379f2c
NotSaqlain <iamnotsaqlain@gmail.com>
2024-03-31 20:31:45 +0200

    update

--- main_2.cpp
+++ main_2.cpp
@@ -5,7 +5,22 @@
 
 using namespace std;
 
+const int N = 10;
+
+void stampa(int array[]) {
+    for (int i = 0; i < N; i++) {
+        cout << array[i] << endl;
+    }
+}
+
 int main() {
-  
-  return 0;
+    int array[N];
+    
+    for (int i = 0; i < N; i++) {
+        array[i] = rand() % 100;
+    }
+    
+    stampa(array);
+
+    return 0;
 }
