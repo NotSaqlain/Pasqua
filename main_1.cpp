0c30cd285631662a9cb2b5b0dd06ad9e82363b9c
NotSaqlain <iamnotsaqlain@gmail.com>
2024-03-31 19:13:09 +0200

    update

--- main_1.cpp
+++ main_1.cpp
@@ -19,20 +19,22 @@ int main() {
     cout << "Scrivi un carattere: ";
     cin >> c;
 
-    cout << c << " e presente " << numOccorenza(s, c) << "volte nella frase!" << endl;
+    cout << c << " è presente " << numOccorenza(s, c) << "volte nella frase!" << endl;
 
     return 0;
 }
 
 
 int numOccorenza(string frase, char c) {
+    int count = 0;
     int start = 0;
     int end = frase.size();
 
     if (start == end) {
         return 0;
     } else if (frase[start] == c) {
+        count = 1;
         frase.erase(0, 1);
-        return numOccorenza(frase, c);
+        return count + numOccorenza(frase, c);
     }
 }
